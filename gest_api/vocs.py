4431faf652cacbacecc68a752af451ff5f3ee522
Ryan Roussel <rroussel@slac.stanford.edu>
2026-07-16 17:10:41 -0500

    Update vocs.py

--- gest_api/vocs.py
+++ gest_api/vocs.py
@@ -388,10 +388,7 @@ class VOCS(BaseModel, validate_assignment=True, arbitrary_types_allowed=True):
     @property
     def bounds(self) -> list:
         """Return the domain bounds for all variables as a list of [lower, upper] pairs."""
-        return [
-            v.domain
-            for _, v in self.variables.items()
-        ]
+        return [v.domain for _, v in self.variables.items()]
 
     @property
     def variable_names(self) -> list[str]:
